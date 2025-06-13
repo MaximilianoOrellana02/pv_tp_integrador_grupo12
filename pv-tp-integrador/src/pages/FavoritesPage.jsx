c9161cd56ac75eb76b38f60ecf0753bfc350a0f5
Maxi2324 <maxloorel5@gmail.com>
2025-06-12 22:02:03 -0300

    Se agrega estilos a pagina de favoritos

--- pv-tp-integrador/src/pages/FavoritesPage.jsx
+++ pv-tp-integrador/src/pages/FavoritesPage.jsx
@@ -1,10 +1,12 @@
 // src/pages/FavoritesPage.jsx
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeFavorite } from '../redux/slices/favoritesSlice'; // Importamos la acción para remover
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeFavorite } from "../redux/slices/favoritesSlice";
+
+import { Link } from "react-router-dom";
+import "./FavoritesPage.css";
 
 const FavoritesPage = () => {
-  // Obtenemos la lista de productos favoritos del estado global de Redux
   const favoriteProducts = useSelector((state) => state.favorites.items);
   const dispatch = useDispatch();
 
@@ -14,21 +16,28 @@ const FavoritesPage = () => {
   };
 
   return (
-    <div>
+    <div className="favorites-container">
       <h1>Mis Productos Favoritos</h1>
       {favoriteProducts.length === 0 ? (
         <p>No tienes productos marcados como favoritos aún.</p>
       ) : (
-        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', gap: '20px' }}>
+        <div className="card-container">
           {favoriteProducts.map((product) => (
-            <div key={product.id} style={{ border: '1px solid #ccc', padding: '15px', borderRadius: '8px', textAlign: 'left' }}>
-              <h3>{product.title}</h3>
-              <p>Categoría: {product.category}</p>
-              <p>Precio: ${product.price}</p>
-              <img src={product.image} alt={product.title} style={{ maxWidth: '100px', height: 'auto', marginBottom: '10px' }} />
-              <button onClick={() => handleRemoveFavorite(product.id)}>
-                Desmarcar como Favorito
-              </button>
+            <div key={product.id} className="product-card">
+              <div className="img-card">
+                <Link to={`/productos/${product.id}`}>
+                  <img src={product.image} alt={product.title} />
+                </Link>
+              </div>
+              <div className="info-card">
+                <Link to={`/productos/${product.id}`} className="product-link">
+                  <h3>{product.title}</h3>
+                </Link>
+                <p>Precio: ${product.price}</p>
+                <button onClick={() => handleRemoveFavorite(product.id)}>
+                  Desmarcar como Favorito
+                </button>
+              </div>
             </div>
           ))}
         </div>
